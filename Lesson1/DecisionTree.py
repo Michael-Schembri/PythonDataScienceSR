f421e0c1fcbd34235bb33fafe95b1e53ffe477bd
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-04-04 12:37:35 +0200

    Experimented with import syntax, changed to preferred version for consistency

--- Lesson1/DecisionTree.py
+++ Lesson1/DecisionTree.py
@@ -1,6 +1,7 @@
-from sklearn import tree
+#from sklearn import tree
+import sklearn.tree as tree
 
-classifier = tree.DecisionTreeClassifier()
+classifier = tree.DecisionTreeClassifier() 
 
 #list of lists, with each list containing [Height (cm), Weight (kg) and Shoe Size (european)]
 trainingSet = [[181, 80, 44], [177, 70, 43], [160, 60, 38],[154, 54, 37], [166, 65, 40],[190, 90, 47], [175, 64, 39],[177, 70, 40], 
@@ -10,7 +11,7 @@ TraininSetLabels = ['male', 'male', 'female', 'female', 'male', 'male', 'female'
  
 classifier = classifier.fit(trainingSet, TraininSetLabels)
 
-prediction = classifier.predict([[182, 70, 45]])
+prediction = classifier.predict([[182, 70, 453]])
 
 
 print(prediction)
